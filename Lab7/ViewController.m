68b86ad87a16f67005e3fd3f69a4a6c22ee72416
Link Arneson <linkarneson@gmail.com>
2017-04-20 19:18:08 -0700

    lab done

--- Lab7/ViewController.m
+++ Lab7/ViewController.m
@@ -50,6 +50,12 @@
     [_Cbrain tangentCalculationString];
     [self updatecalculationView];
 }
+
+- (IBAction)sine:(id)sender {
+    [_Cbrain sineCalculationString];
+    [self updatecalculationView];
+}
+
 - (IBAction)actionPressed:(id)sender {
     UIButton *pressedButton = (UIButton *)sender;
     if ([_Cbrain recognizeAction:(int)pressedButton.tag]) {
